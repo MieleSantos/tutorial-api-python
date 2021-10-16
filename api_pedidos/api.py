02cdf033f72116b846975809d534d2d3063f75b9
mielesantos <mielesnts@gmail.com>
2021-10-16 17:23:24 -0300

    Adiciona listagem de itens em um pedido

--- api_pedidos/api.py
+++ api_pedidos/api.py
@@ -1,9 +1,38 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends, Request
+from api_pedidos.esquema import Item
+from typing import List
+from uuid import UUID
+from fastapi.responses import JSONResponse
+from api_pedidos.excecao import PedidoNaoEncontradoError, FalhaDeComunicacaoError
+from http import HTTPStatus
+from api_pedidos.magalu_api import recuperar_itens_por_pedido
 
 app = FastAPI()
+
+
+def recuperar_itens_por_pedido(identificacao_do_pedido: UUID) -> List[Item]:
+    pass
+
+
+@app.exception_handler(PedidoNaoEncontradoError)
+def tratar_erro_pedido_nao_encontrado(
+        request: Request, exc: PedidoNaoEncontradoError):
+    return JSONResponse(
+        status_code=HTTPStatus.NOT_FOUND,
+        content={"message": "Pedido não encontrado"}
+        )
+
+
+@app.exception_handler(FalhaDeComunicacaoError)
+def tratar_erro_falha_de_comunicacao(request: Request, exc: FalhaDeComunicacaoError):
+    return JSONResponse(status_code=HTTPStatus.BAD_GATEWAY, content={"message": "Falha de comunicação com o servidor remoto"})
 
 
 @app.get("/healthcheck")
 async def healthcheck():
     return {"status": "ok"}
+
+
+@app.get("/orders/{identificacao_do_pedido}/items")
+def listar_itens(itens: List[Item] = Depends(recuperar_itens_por_pedido)):
+    return itens
